09df1c8d9f5c408318400d66792493b5e749175e
koen-benne <koen.benne@gmail.com>
2024-02-29 17:46:57 +0100

    moving stuff from home manager to global

--- common/packages/home.nix
+++ common/packages/home.nix
@@ -4,21 +4,16 @@
   home.packages = with pkgs; [
     (pass.withExtensions (ext: [ ext.pass-otp ]))
     age
-    gcc
-    curl
     diffutils
     stow
     file
     findutils
     gawk
-    gnupg
     gzip
     btop
     imagemagick
     ipcalc
     jq
-    man
-    openssh
     python3
     ripgrep
     scripts
