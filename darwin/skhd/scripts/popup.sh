dbfa12c8693647f32d7f058bffc4c26b5f14b3e5
koen-benne <koen.benne@gmail.com>
2024-02-29 11:33:15 +0100

    start on updating popup script

--- darwin/skhd/scripts/popup.sh
+++ darwin/skhd/scripts/popup.sh
@@ -1,3 +1,41 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+# does not work because window is not managed
 
-kitty -T "kittypopup" -o initial_window_width=60c -o initial_window_height=35c -o remember_window_size=no -e $1
+window_title="Fzf"
+width=50
+height=20
+
+screen_width=$(yabai -m query --displays --display | jq -r '.frame.w')
+screen_height=$(yabai -m query --displays --display | jq -r '.frame.h')
+
+center_x=$(( ${screen_width%%.*} / 2 ))
+center_y=$(( ${screen_height%%.*} / 2 ))
+
+alacritty -o window.dimensions.columns=$width -o window.dimensions.lines=$height \
+    --title=Fzf --command "$1" &
+
+sleep 1
+
+# Loop over all windows
+yabai -m query --windows | jq -r '.[] | "\(.id) \(.app)"' | while read -r line ; do
+  id=$(echo $line | cut -f1 -d' ')
+  title=$(echo $line | cut -f2 -d' ')
+  echo $title
+
+  # If the window's title matches the title you're looking for
+  if [[ $title == $window_title ]]; then
+    # Get the window's width and height
+    window_width=$(yabai -m query --windows --window $id | jq -re ".frame.w")
+    window_height=$(yabai -m query --windows --window $id | jq -re ".frame.h")
+
+    # Calculate the position for the window
+    pos_x=$(( (${screen_width%%.*} - window_width) / 2 ))
+    pos_y=$(( (${screen_height%%.*} - window_height) / 2 ))
+
+    # Move the window to the calculated position
+    yabai -m window $id --move abs:$pos_x:$pos_y
+
+    # Stop the loop
+    break
+  fi
+done
