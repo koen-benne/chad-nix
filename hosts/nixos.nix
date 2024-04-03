595c67cc473d10148b8390f90d44f7250f6d1d31
koen-benne <koen.benne@gmail.com>
2024-04-03 10:56:28 +0200

    enable firewall on nixos

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -4,7 +4,21 @@
   imports = [
     ./hardware-nixos.nix
   ];
-  networking.hostName = "nixos";
+  networking = {
+    hostName = "nixos";
+   # usePredictableInterfaceNames = true;
+   # defaultGateway = "192.168.0.1";
+   # nameservers = ["8.8.8.8" "8.8.4.4"];
+   firewall = {
+     enable = true;
+     allowedTCPPorts = [ 80 443 53317 22067 22070 ]; # 53317 is for LocalSend and 22067, 22070 are for Syncthing
+     allowedUDPPortRanges = [
+       { from = 4000; to = 4007; }
+       { from = 53315; to = 53318; }
+       { from = 8000; to = 8010; }
+     ];
+   };
+  };
 
   my.desktop.enable = true;
   my.steam.enable = true;
