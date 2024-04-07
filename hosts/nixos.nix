cf6d708fa7e103aa5ff882b647468a54b7b6a9ae
koen-benne <koen.benne@gmail.com>
2024-04-07 12:52:38 +0200

    update firewall

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -9,16 +9,16 @@
    # usePredictableInterfaceNames = true;
    # defaultGateway = "192.168.0.1";
    # nameservers = ["8.8.8.8" "8.8.4.4"];
-   firewall = {
-     enable = true;
-     allowedTCPPorts = [ 80 443 53317 22067 22070 8211 ]; # 53317 is for LocalSend and 22067, 22070 are for Syncthing and 8211 is for palworld server
-     allowedUDPPorts = [ 8211 ];
-     allowedUDPPortRanges = [
-       { from = 4000; to = 4007; }
-       { from = 53315; to = 53318; }
-       { from = 8000; to = 8010; }
-     ];
-   };
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 80 443 53317 22067 22070 8211 ]; # 53317 is for LocalSend and 22067, 22070 are for Syncthing and 8211 is for palworld server
+      allowedUDPPorts = [ 8211 ];
+      allowedUDPPortRanges = [
+        { from = 4000; to = 4007; }
+        { from = 53315; to = 53318; }
+        { from = 8000; to = 8010; }
+      ];
+    };
   };
 
   my.desktop.enable = true;
