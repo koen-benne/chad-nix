0794fd1339674e1b5c51608b2cc2148c8d79e2cb
koen-benne <koen.benne@gmail.com>
2024-04-07 12:54:52 +0200

    fix suspend
    f

--- nixos/hyprland/scripts/suspend.sh
+++ nixos/hyprland/scripts/suspend.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-swaylock
-sleep 1
 # openrgb --profile ../../openrgb/profiles/asleep.orp
-# sleep 1
-systemctl suspend
-# sleep 1
+swaylock --daemonize && systemctl suspend
 # openrgb --profile ../../openrgb/profiles/awake.orp
