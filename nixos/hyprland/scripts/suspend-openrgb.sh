6ec676b76ebf3b50c20ce1766fa93690bd640e0b
koen-benne <koen.benne@gmail.com>
2024-04-07 12:52:31 +0200

    update suspend

--- nixos/hyprland/scripts/suspend-openrgb.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-openrgb --profile ../../openrgb/profiles/asleep.orp
-sleep 1
-systemctl suspend
-sleep 1
-openrgb --profile ../../openrgb/profiles/awake.orp
---
+++ nixos/hyprland/scripts/suspend.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+swaylock
+sleep 1
+# openrgb --profile ../../openrgb/profiles/asleep.orp
+# sleep 1
+systemctl suspend
+# sleep 1
+# openrgb --profile ../../openrgb/profiles/awake.orp
