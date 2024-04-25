e95276a18b17ba4201f4c6e50acaf6113119fc48
koen-benne <koen.benne@gmail.com>
2024-04-25 19:46:42 +0200

    update

--- common/home.nix
+++ common/home.nix
@@ -1,9 +1,10 @@
-# The whole "my" section needs to go, it can be defined in different places instead
+#tThe whole "my" section needs to go, it can be defined in different places instead
 { inputs, config, lib, pkgs, ... }:
 
 {
   imports = [
     inputs.nix-index-database.hmModules.nix-index
+    ./my/default.nix
   ] ++ lib.my.getHmModules [ ./. ];
 
   my = {
--- common/my/default.nix
+++ common/my/default.nix
@@ -10,13 +10,11 @@ with lib;
     workmail = mkOption { type = types.str; };
     uid = mkOption { type = types.int; };
   };
-  config = {
-    my = {
-      user = "koenbenne";
-      name = "Koen Benne";
-      email = "koen.benne@gmail.com";
-      workmail = "koen.benne@iodigital.com";
-      uid = 1000;
-    };
+  config.my = {
+    user = "koenbenne";
+    name = "Koen Benne";
+    email = "koen.benne@gmail.com";
+    workmail = "koen.benne@iodigital.com";
+    uid = 1000;
   };
 }
