a60ed0c98fcc50898b193cf93690bfeb4462370d
koen-benne <koen.benne@gmail.com>
2024-05-03 14:27:11 +0200

    update overlays self and super to final and prev

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,14 +1,12 @@
-# https://discourse.nixos.org/t/in-overlays-when-to-use-self-vs-super/2968/12
-
-self: super: {
+final: prev: {
   # pkgs
-  # grub-reboot-menu = self.callPackage ../pkgs/grub-reboot-menu { };
-  jetbrains-mono-nerdfont = self.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
-  nibar = self.callPackage ../pkgs/nibar { };
-  scripts = self.callPackage ../pkgs/scripts { };
-  sf-symbols = self.sf-symbols-minimal;
-  sf-symbols-app = self.callPackage ../pkgs/sf-symbols { app = true; fonts = false; };
-  sf-symbols-full = self.callPackage ../pkgs/sf-symbols { full = true; };
-  sf-symbols-minimal = self.callPackage ../pkgs/sf-symbols { };
-  # subfinder = self.callPackage ../pkgs/subfinder { };
+  # grub-reboot-menu = final.callPackage ../pkgs/grub-reboot-menu { };
+  jetbrains-mono-nerdfont = final.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+  nibar = final.callPackage ../pkgs/nibar { };
+  scripts = final.callPackage ../pkgs/scripts { };
+  sf-symbols = final.sf-symbols-minimal;
+  sf-symbols-app = final.callPackage ../pkgs/sf-symbols { app = true; fonts = false; };
+  sf-symbols-full = final.callPackage ../pkgs/sf-symbols { full = true; };
+  sf-symbols-minimal = final.callPackage ../pkgs/sf-symbols { };
+  # subfinder = final.callPackage ../pkgs/subfinder { };
 }
