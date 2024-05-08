25cc087f86ce9f0c32a5eb3f3eb8293d04efdd10
koen-benne <koen.benne@gmail.com>
2024-05-08 19:41:53 +0200

    add opencl support
    s

--- hosts/hardware-nixos.nix
+++ hosts/hardware-nixos.nix
@@ -37,4 +37,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.opengl.extraPackages = [
+    pkgs.rocm-opencl-icd
+  ];
 }
