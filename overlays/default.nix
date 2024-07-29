9de39e98a82b9fc6811ed1e7b8eab15a770c7d54
Koen Benne <koen.benne@gmail.com>
2024-07-29 12:32:34 +0200

    remove sf-symbols

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,12 +5,5 @@ final: prev: {
   openASAR = final.discord.override {withOpenASAR = true;};
   nibar = final.callPackage ../pkgs/nibar {};
   scripts = final.callPackage ../pkgs/scripts {};
-  sf-symbols = final.sf-symbols-minimal;
-  sf-symbols-app = final.callPackage ../pkgs/sf-symbols {
-    app = true;
-    fonts = false;
-  };
-  sf-symbols-full = final.callPackage ../pkgs/sf-symbols {full = true;};
-  sf-symbols-minimal = final.callPackage ../pkgs/sf-symbols {};
   # subfinder = final.callPackage ../pkgs/subfinder { };
 }
