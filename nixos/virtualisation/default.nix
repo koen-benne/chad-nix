8b8d7ee46ccded01730e7ce0391c1ff44fd2851b
koen-benne <koen.benne@gmail.com>
2024-08-16 11:52:35 +0200

    fix kvm stuff

--- nixos/virtualisation/default.nix
+++ nixos/virtualisation/default.nix
@@ -13,12 +13,11 @@ in {
 
   config = mkIf cfg.enable {
     virtualisation.libvirtd.enable = true;
-    users.extraUsers.${config.my.user}.extraGroups = [ "libvirtd" ];
+    users.extraUsers.${config.my.user}.extraGroups = [ "kvm" "libvirtd" "input" ];
 
     boot.extraModprobeConfig = ''
-      options kvm_intel nested=1
-      options kvm_intel emulate_invalid_guest_state=0
-      options kvm ignore_msrs=1
+      options kvm_amd nested=1
+      options kvm ignore_msrs=1 report_ignored_msrs=0
     '';
   };
 }
