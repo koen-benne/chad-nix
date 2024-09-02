f03e2f15c29360dccf4f055fd30d547a7c9cac1d
Koen Benne <koen.benne@gmail.com>
2024-09-02 11:40:02 +0200

    update styling of wezterm
    s

--- modules/darwin/wezterm/home.nix
+++ modules/darwin/wezterm/home.nix
@@ -23,9 +23,35 @@ in {
         config.hide_tab_bar_if_only_one_tab = true
         config.window_background_opacity = 0.6
         config.macos_window_background_blur = 20
+
+        config.default_cursor_style = 'SteadyBar'
+
         config.colors = {
           foreground = "#ffffff",
-          background = "#000408"
+          background = "#000408",
+          cursor_border = "#c0cdc7",
+
+          ansi = {
+            '#073642', -- black
+            '#dc322f', -- maroon
+            '#859900', -- green
+            '#b58900', -- olive
+            '#268bd2', -- navy
+            '#d33682', -- purple
+            '#2aa198', -- teal
+            '#eee8d5', -- silver
+          },
+
+          brights = {
+            '#08404f', -- grey
+            '#e35f5c', -- red
+            '#9fb700', -- lime
+            '#d9a400', -- yellow
+            '#4ba1de', -- blue
+            '#dc619d', -- fuchsia
+            '#32c1b6', -- aqua
+            '#ffffff', -- white
+          }
         }
         config.font = wezterm.font { family = "JetBrainsMonoNL Nerd Font Mono" }
 
