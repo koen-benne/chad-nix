104d6c11eb2e34038eeafe0d9385b3fa02a89304
koen-benne <koen.benne@gmail.com>
2024-10-08 19:17:47 +0200

    add configs

--- modules/darwin/aerospace/home.nix
+++ modules/darwin/aerospace/home.nix
@@ -1,11 +1,18 @@
 {
   inputs,
   pkgs,
+  lib,
   ...
 }: {
   home.packages = [
     inputs.aerospace.packages.aarch64-darwin.default
-    pkgs.sketchybar
-    pkgs.sketchybar-app-font
   ];
+
+  # home.activation.aerospace = lib.hm.dag.entryAfter ["writeBoundary"] ("open ${inputs.aerospace.packages.aarch64-darwin.default}/AeroSpace.app");
+  home.file = {
+    ".config/sketchybar".source = ./sketchybar-config;
+  };
+  home.file = {
+    ".config/aerospace".source = ./aerospace-config;
+  };
 }
