cf3908e82ba324c4bc193458bdd70f40b7cf36b4
koen-benne <koen.benne@gmail.com>
2024-11-03 15:17:00 +0100

    add creative server

--- modules/nixos/minecraft/default.nix
+++ modules/nixos/minecraft/default.nix
@@ -22,8 +22,32 @@
         package = pkgs.fabric-server;
 
         serverProperties = {
+          motd = "Tnauwiecraft";
           white-list = true;
           server-port = 25565;
+          difficulty = "normal";
+          gamemode = "survival";
+          max-players = 3;
+          online-mode = true;
+          reduced-debug-info = false;
+        };
+
+        whitelist = {
+          YerBoyCone = "ef4f6857-d513-4dd5-8595-cd9711743a55";
+        };
+
+        jvmOpts = "-Xmx4G -Xms4G";
+        symlinks = {
+        };
+      };
+      tnauwiecreative = {
+        enable = true;
+        package = pkgs.fabric-server;
+
+        serverProperties = {
+          motd = "Tnauwiecreative";
+          white-list = true;
+          server-port = 25566;
           difficulty = "normal";
           gamemode = "survival";
           max-players = 3;
