89e69287dce277cc952ebc67642d084ee53e68ee
koen-benne <koen.benne@gmail.com>
2024-11-23 13:25:52 +0100

    use unstable blueman

--- modules/nixos/bluetooth/default.nix
+++ modules/nixos/bluetooth/default.nix
@@ -15,6 +15,9 @@ in {
     hardware.bluetooth.enable = true;
     hardware.bluetooth.powerOnBoot = true;
 
-    services.blueman.enable = true;
+    services.blueman = {
+      enable = true;
+      package = pkgs.unstable.blueman;
+    };
   };
 }
