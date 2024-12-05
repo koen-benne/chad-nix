3d15010df80f4caa7855adddfdb6cbc83dc19463
koen-benne <koen.benne@gmail.com>
2024-12-05 13:08:51 +0100

    set up hyprlock

--- modules/nixos/lockscreen/home.nix
+++ modules/nixos/lockscreen/home.nix
@@ -13,16 +13,6 @@ in {
   config = mkIf cfg.enable {
     programs.hyprlock = {
       enable = true;
-      settings = {
-        show-user-name = true;
-        show-date = true;
-        show-seconds = true;
-        show-indicators = true;
-        indicator-radius = 10;
-        indicator-thickness = 2;
-        caps-lock-indicator-thickness = 2;
-        caps-lock-indicator-radius = 10;
-      };
     };
   };
 }
--- modules/nixos/theme/home.nix
+++ modules/nixos/theme/home.nix
@@ -24,5 +24,78 @@ in {
         terminal = 0.7;
       };
     };
+    programs.hyprlock.settings = {
+      background = {
+        blur_passes = 3;
+        blur_size = 10;
+      };
+      input-field = {
+        size = "250, 50";
+        outline_thickness = 3;
+        dots_size = 0.33;
+        dots_spacing = 0.15;
+        dots_center = true;
+        placeholder_text = ''<i>Password...</i>'';
+        hide_input = false;
+        position = "0, 200";
+        halign = "center";
+        valign = "bottom";
+      };
+      # image = {
+      #   path = "${config.stylix.image}";
+      #   size = "230, 250";
+      #   rounding = -1;
+      #   border_size = 2;
+      #   rotate = 0;
+      #   reload_time = -1;
+      #   position = "0, 300";
+      #   halign = "center";
+      #   valign = "bottom";
+      # };
+      label = [
+        {
+          text = ''cmd[update:18000000] echo "<b> "$(date +'%A, %-d %B %Y')" </b>"'';
+          font_size = 34;
+          position = "0, -150";
+          halign = "center";
+          valign = "top";
+        }
+        {
+          text = ''cmd[update:18000000] echo "<b> "$(date +'Week %U')" </b>"'';
+          font_size = 24;
+          position = "0, -250";
+          halign = "center";
+          valign = "top";
+        }
+        {
+          text = ''cmd[update:1000] echo "<b><big> $(date +"%H:%M:%S") </big></b>"'';
+          font_size = 94;
+          position = "0, 0";
+          halign = "center";
+          valign = "center";
+        }
+        {
+          text = "   $USER";
+          font_size = 18;
+          position = "0, 100";
+          halign = "center";
+          valign = "bottom";
+        }
+        {
+          text = ''cmd[update:60000] echo "<b> "$(uptime -p || $Scripts/UptimeNixOS.sh)" </b>"'';
+          font_size = 24;
+          position = "0, 0";
+          halign = "right";
+          valign = "bottom";
+        }
+        {
+          text = ''cmd[update:3600000] [ -f ~/.cache/.weather_cache ] && cat  ~/.cache/.weather_cache'';
+          font_size = 24;
+          position = "50, 0";
+          halign = "left";
+          valign = "bottom";
+        }
+      ];
+    };
   };
 }
