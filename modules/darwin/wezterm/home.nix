0a9e85384f49fedc9b194e2be684f8be94f47946
Koen Benne <koen.benne@gmail.com>
2024-12-05 17:32:32 +0100

    allow stylix to manage window background opacity

--- modules/darwin/wezterm/home.nix
+++ modules/darwin/wezterm/home.nix
@@ -23,7 +23,6 @@ in {
         config.front_end = "WebGpu"
         config.window_decorations = "RESIZE"
         config.hide_tab_bar_if_only_one_tab = true
-        config.window_background_opacity = 0.6
         config.macos_window_background_blur = 20
 
         config.default_cursor_style = 'SteadyBar'
