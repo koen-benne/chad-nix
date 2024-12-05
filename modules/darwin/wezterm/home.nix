188e793468f75904bcfa639f8b263645add0027c
Koen Benne <koen.benne@gmail.com>
2024-12-05 16:54:57 +0100

    fix wezterm issue

--- modules/darwin/wezterm/home.nix
+++ modules/darwin/wezterm/home.nix
@@ -19,6 +19,8 @@ in {
         local config = wezterm.config_builder()
 
         config.font_size = 15.0
+        # Fixes https://github.com/wez/wezterm/issues/6005
+        config.front-end = "WebGpu"
         config.window_decorations = "RESIZE"
         config.hide_tab_bar_if_only_one_tab = true
         config.window_background_opacity = 0.6
