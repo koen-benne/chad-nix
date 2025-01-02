580cef51aea50ca107e4305bec55017a5a8db67e
koen-benne <koen.benne@gmail.com>
2025-01-02 09:39:24 +0100

    remove nibar and subfinder

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,5 @@ final: prev: {
   # pkgs
   jetbrains-mono-nerdfont = final.nerdfonts.override {fonts = ["JetBrainsMono"];};
   openASAR = final.discord.override {withOpenASAR = true;};
-  nibar = final.callPackage ../pkgs/nibar {};
   scripts = final.callPackage ../pkgs/scripts {};
-  # subfinder = final.callPackage ../pkgs/subfinder { };
 }
