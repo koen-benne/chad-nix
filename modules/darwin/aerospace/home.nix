13b33c7f04eb7e53abbf97b6907d52d041fdd4c3
Koen Benne <koen.benne@gmail.com>
2025-01-02 16:47:00 +0100

    finallyyyyyy jesus christ

--- modules/darwin/aerospace/home.nix
+++ modules/darwin/aerospace/home.nix
@@ -115,27 +115,14 @@ in {
           fi'
         ''''
 
-        alt-w = ''''exec-and-forget osascript -e '
-        -- Check if Zen Browser is already running
-        set isZenBrowserRunning to false
-        tell application "System Events"
-            if (exists process "Zen Browser") then
-                set isZenBrowserRunning to true
-            end if
-        end tell
-
-        -- If Zen Browser is not running, open it
-        if not isZenBrowserRunning then
-            tell application "Zen Browser" to activate
-        else
-            -- If Zen Browser is running, create a new window
-            tell application "Zen Browser"
-                activate
-            end tell
-            tell application "System Events"
-              keystroke "n" using {command down}
-            end tell
-        end if'
+        alt-w = ''''exec-and-forget bash -c '
+          if ps aux | grep -i "Zen Browser" | grep -v "grep" > /dev/null
+          then
+            osascript -e "tell application \"Zen Browser\" to activate"
+            osascript -e "tell application \"System Events\" to keystroke \"n\" using command down"
+          else
+            open -a "Zen Browser"
+          fi'
         ''''
 
         alt-q = 'close'
