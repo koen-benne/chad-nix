7d361b0ecb9a2e1eacfb3034f1fb074ac6a74151
koen-benne <koen.benne@gmail.com>
2025-07-06 22:57:48 +0200

    updating..

--- modules/darwin/theme/home.nix
+++ modules/darwin/theme/home.nix
@@ -16,23 +16,28 @@ in {
     inputs.stylix.homeModules.stylix
   ];
 
-  config = mkIf cfg.enable {
-    stylix = {
-      enable = true;
-      # We have readonly packages
-      overlays.enable = false;
-      image = ../../../assets/wp-normal.png;
-      targets = {
-        neovim.enable = false;
-        fzf.enable = false;
-        spicetify.enable = false;
+  config = mkMerge [
+    {
+      # We can never have overlays that are not in parts/overlays.nix (read-only)
+      stylix.overlays.enable = false;
+    }
+    (mkIf cfg.enable {
+      stylix = {
+        enable = true;
+        # We have readonly packages
+        image = ../../../assets/wp-normal.png;
+        targets = {
+          neovim.enable = false;
+          fzf.enable = false;
+          spicetify.enable = false;
+        };
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/railscasts.yaml";
+        polarity = "dark";
+        opacity = {
+          popups = 0.8;
+          terminal = 0.7;
+        };
       };
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/railscasts.yaml";
-      polarity = "dark";
-      opacity = {
-        popups = 0.8;
-        terminal = 0.7;
-      };
-    };
-  };
+    })
+  ];
 }
--- modules/nixos/gaming/default.nix
+++ modules/nixos/gaming/default.nix
@@ -5,7 +5,7 @@
   inputs,
   ...
 }: let
-  inherit (lib) mdDoc mkEnableOption mkIf;
+  inherit (lib) mdDoc mkEnableOption mkIf mkMerge;
   cfg = config.my.gaming;
 in {
   options.my.gaming = {
@@ -19,82 +19,87 @@ in {
     inputs.chaotic.nixosModules.nyx-registry
   ];
 
-  config = mkIf cfg.enable {
-    chaotic.nyx.overlay.enable = false;
-    boot.kernelPackages = pkgs.linuxPackages_cachyos;
+  config = mkMerge [
+    {
+      # We can never have overlays that are not in parts/overlays.nix (read-only)
+      chaotic.nyx.overlay.enable = false;
+    }
+    (mkIf cfg.enable {
+      boot.kernelPackages = pkgs.linuxPackages_cachyos;
 
-    hm.my.gaming.enable = true;
-    # environment.systemPackages = [ pkgs.steam-run-native ];
+      hm.my.gaming.enable = true;
+      # environment.systemPackages = [ pkgs.steam-run-native ];
 
-    environment.systemPackages = with pkgs; [
-      steamcmd
-      mangohud
-      xdg-user-dirs
-      protonup-qt
+      environment.systemPackages = with pkgs; [
+        steamcmd
+        mangohud
+        xdg-user-dirs
+        protonup-qt
 
-      steamtinkerlaunch
-    ];
+        steamtinkerlaunch
+      ];
 
-    programs.nix-ld.enable = true;
-    programs.nix-ld.libraries = with pkgs; [
-      stdenv.cc.cc.lib
-    ];
+      programs.nix-ld.enable = true;
+      programs.nix-ld.libraries = with pkgs; [
+        stdenv.cc.cc.lib
+      ];
 
-    programs.gamemode.enable = true;
+      programs.gamemode.enable = true;
 
-    programs.steam = {
-      enable = true;
-    };
-
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
-      # Use mesa-git instead of stable mesa (for FSR4 and HDR)
-      # Might want to go back to the stable version once it supports all the stuff that I want
-      package = pkgs.mesa_git;
-      package32 = pkgs.mesa32_git;
-    };
-
-    hardware.steam-hardware.enable = true;
-
-    services.sunshine = mkIf cfg.enableSunshine {
-      enable = true;
-      autoStart = true;
-      capSysAdmin = true;
-      openFirewall = true;
-      # Currently, it is necessary to run 'sudo setfacl -m g:input:rw /dev/uhid' to get ds5 emu to work.
-      # Seems like https://github.com/LizardByte/Sunshine/pull/2906 should fix this issue.
-      settings = {
-        gamepad = "ds5";
+      programs.steam = {
+        enable = true;
       };
-      applications = {
-        env = {
-          PATH = "$(PATH):$(HOME)\/.local\/bin";
+
+      hardware.graphics = {
+        enable = true;
+        enable32Bit = true;
+        # Use mesa-git instead of stable mesa (for FSR4 and HDR)
+        # Might want to go back to the stable version once it supports all the stuff that I want
+        package = pkgs.mesa_git;
+        package32 = pkgs.mesa32_git;
+      };
+
+      hardware.steam-hardware.enable = true;
+
+      services.sunshine = mkIf cfg.enableSunshine {
+        enable = true;
+        autoStart = true;
+        capSysAdmin = true;
+        openFirewall = true;
+        # Currently, it is necessary to run 'sudo setfacl -m g:input:rw /dev/uhid' to get ds5 emu to work.
+        # Seems like https://github.com/LizardByte/Sunshine/pull/2906 should fix this issue.
+        settings = {
+          gamepad = "ds5";
         };
-        apps = [
-          {
-            name = "Desktop";
-            image-path = "desktop.png";
-          }
-          {
-            name = "Steam Big Picture";
-            detached = [
-              "${pkgs.util-linux}/bin/setsid ${pkgs.steam}/bin/steam steam:\/\/open\/gamepadui"
-            ];
-            image-path = "steam.png";
-            exclude-global-prep-cmd = "";
-            auto-detach = "true";
-            wait-all = "true";
-            exit-timeout = "5";
-            prep-cmd = [
-              {
-                do = ''${pkgs.bash}/bin/bash -c "${pkgs.hyprland}/bin/hyprctl keyword monitor \"DP-3,''${SUNSHINE_CLIENT_WIDTH}x''${SUNSHINE_CLIENT_HEIGHT}@''${SUNSHINE_CLIENT_FPS},0x0,1\""'';
-                undo = "${pkgs.hyprland}/bin/hyprctl keyword monitor DP-3,3440x1440@159.96201,0x0,1";
-              }
-            ];
-          }
-        ];
+        applications = {
+          env = {
+            PATH = "$(PATH):$(HOME)\/.local\/bin";
+          };
+          apps = [
+            {
+              name = "Desktop";
+              image-path = "desktop.png";
+            }
+            {
+              name = "Steam Big Picture";
+              detached = [
+                "${pkgs.util-linux}/bin/setsid ${pkgs.steam}/bin/steam steam:\/\/open\/gamepadui"
+              ];
+              image-path = "steam.png";
+              exclude-global-prep-cmd = "";
+              auto-detach = "true";
+              wait-all = "true";
+              exit-timeout = "5";
+              prep-cmd = [
+                {
+                  do = ''${pkgs.bash}/bin/bash -c "${pkgs.hyprland}/bin/hyprctl keyword monitor \"DP-3,''${SUNSHINE_CLIENT_WIDTH}x''${SUNSHINE_CLIENT_HEIGHT}@''${SUNSHINE_CLIENT_FPS},0x0,1\""'';
+                  undo = "${pkgs.hyprland}/bin/hyprctl keyword monitor DP-3,3440x1440@159.96201,0x0,1";
+                }
+              ];
+            }
+          ];
+        };
       };
-    };
-  };
+    })
+  ];
 }
