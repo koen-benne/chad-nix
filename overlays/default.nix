12b8c942e7ba6803af514f515167098605c3b47f
koen-benne <koen.benne@gmail.com>
2025-09-04 12:12:35 +0200

    opforjellyfin overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -24,4 +24,55 @@ final: prev: {
       runHook postInstall
     '';
   });
+  opforjellyfin = prev.stdenv.mkDerivation rec {
+    pname = "opforjellyfin";
+    version = "1.0.1";
+
+    src = prev.fetchurl {
+      url = "https://github.com/tissla/opforjellyfin/releases/download/v${version}/opfor-linux";
+      sha256 = "1w6bm1j325nv964yg8w4hbnskv1gwdw1arhfy9fpp4pjn0w92i16";
+    };
+
+    nativeBuildInputs = with prev; [
+      autoPatchelfHook
+      makeWrapper
+    ];
+
+    buildInputs = with prev; [
+      # C/C++ runtime libraries
+      stdenv.cc.cc.lib      # This provides libstdc++.so.6
+      gcc.cc.lib            # Alternative way to get libstdc++
+      glibc                 # C standard library
+    ];
+
+    # Runtime dependencies
+    propagatedBuildInputs = with prev; [
+      git  # Required for the program to function
+    ];
+
+    dontUnpack = true;
+    dontBuild = true;
+
+    installPhase = ''
+      runHook preInstall
+      install -D -m755 $src $out/bin/opfor-linux
+      # Create a symlink with a shorter name
+      ln -s $out/bin/opfor-linux $out/bin/opfor
+      runHook postInstall
+    '';
+
+    postFixup = ''
+      # Additional wrapping if needed for runtime dependencies
+      # wrapProgram $out/bin/opfor-linux \
+      #   --prefix PATH : ${prev.lib.makeBinPath [ /* runtime dependencies */ ]}
+    '';
+
+    meta = with prev.lib; {
+      description = "opforjellyfin - A tool for Jellyfin";
+      homepage = "https://github.com/tissla/opforjellyfin";
+      license = licenses.unfree; # Update this based on actual license
+      maintainers = [ ]; # Add your info if desired
+      platforms = platforms.linux;
+    };
+  };
 }
