f6e45d344a25e466a98c0e39a1a88e455efeab22
Koen Benne <koen.benne@gmail.com>
2025-11-14 00:01:30 +0100

    update

--- modules/nixos/theme/compat.nix
+++ modules/nixos/theme/compat.nix
@@ -1,7 +1,11 @@
-{lib, ...}: let
+{lib, inputs, ...}: let
   inherit (lib) mkEnableOption;
 in {
   options.my.theme = {
     enable = mkEnableOption "system theme configuration";
   };
+
+  imports = [
+    inputs.stylix.homeModules.stylix
+  ];
 }
