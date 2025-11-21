d16e355ca1fa87748c5a1430fa8d9c4d266180cb
Koen Benne <koen.benne@gmail.com>
2025-11-21 11:59:40 +0100

    a

--- modules/common/zen-browser/home.nix
+++ modules/common/zen-browser/home.nix
@@ -4,9 +4,13 @@
   sys,
   ...
 }: let
-  inherit (lib) mkIf;
+  inherit (lib) mdDoc mkEnableOption mkIf;
   cfg = sys.my.zen-browser;
 in {
+  options.my.zen-browser = {
+    enable = mkEnableOption (mdDoc "zen-browser");
+  };
+
   imports = [
     inputs.zen-browser.homeModules.beta
   ];
