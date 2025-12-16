50df11b877a8269bee5ca8542850f426f66b27f8
Koen Benne <koen.benne@gmail.com>
2025-12-16 20:17:22 +0100

    fixy

--- modules/common/opencode/bonzai-remove-unsupported-params.ts
+++ modules/common/opencode/bonzai-remove-unsupported-params.ts
@@ -3,7 +3,7 @@ import type { Plugin } from "@opencode-ai/plugin"
 export const BonzaiRemoveUnsupportedParams: Plugin = async () => {
   return {
     async "chat.params"({ provider, model }, output) {
-      if (provider.info.id.includes("bonzai") && model.id.includes("gpt-5")) {
+      if (provider.id.includes("bonzai") && model.id.includes("gpt-5")) {
         output.options["textVerbosity"] = undefined
       }
     }
