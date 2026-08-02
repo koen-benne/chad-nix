a7c9def53a6f7b520904b58b3b17803373b5c27e
Koen Benne <koen.benne@gmail.com>
2026-08-02 10:35:24 +0200

    move zellij package.nix to default.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,7 @@
 final: prev: {
   # Pin zellij to 0.43.1: terminal transparency broken since 0.44 (zellij-org/zellij#5175).
   # Remove this entry (and pkgs/zellij/) once fixed upstream.
-  zellij = final.callPackage ../pkgs/zellij/package.nix {};
+  zellij = final.callPackage ../pkgs/zellij {};
 
   # Override quickshell from unstable to be available in stable
   quickshell = final.unstable.quickshell;
